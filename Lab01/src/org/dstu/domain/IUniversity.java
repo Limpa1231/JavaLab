56f0bd612288ba126da54d4e9a6a751efb314499
ZdenisZ <90981641+Limpa1231@users.noreply.github.com>
2024-10-27 22:18:33 +0300

    Delete Lab01 directory

--- Lab01/src/org/dstu/domain/IUniversity.java
+++
@@ -1,9 +0,0 @@
-package org.dstu.domain;
-
-import java.io.Serializable;
-
-public interface IUniversity extends Serializable {
-    default void printInfo() {
-        System.out.println(this);
-    }
-}
